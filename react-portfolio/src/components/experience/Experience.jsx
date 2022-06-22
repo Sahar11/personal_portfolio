04d5bc642c149e61baa9aa4013c36d75b4df8b53
Sahar11 <saharhalim151@gmail.com>
2022-06-21 22:03:33 -0400

    add styles & content to components

--- react-portfolio/src/components/experience/Experience.jsx
+++ react-portfolio/src/components/experience/Experience.jsx
@@ -3,7 +3,22 @@ import './experience.css';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id="experience">
+      <h5>What skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+             <article className='experience__details'>
+
+             </article>
+          </div>
+        </div>
+        <div className="experience__backend"></div>
+      </div>
+    </section>
   )
 }
 
